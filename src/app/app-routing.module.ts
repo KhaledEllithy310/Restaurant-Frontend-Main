67fab0345ce5ff40b7e8f584f4bbcc0392e69514
KhaledEllithy310 <khaledellessy310@gmail.com>
2023-06-24 00:31:20 +0300

    Create waiter Component

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,10 +1,25 @@
-import { NgModule } from '@angular/core';
-import { RouterModule, Routes } from '@angular/router';
+import { NgModule } from "@angular/core";
+import { RouterModule, Routes } from "@angular/router";
+import { WaiterComponent } from "./waiter/waiter.component";
 
-const routes: Routes = [];
-
+const routes: Routes = [
+  {
+    path: "",
+    redirectTo: "register",
+    pathMatch: "full",
+  },
+  {
+    path: "home",
+    component: WaiterComponent,
+  },
+  {
+    path: "Waiter",
+    loadChildren: () =>
+      import("./waiter/waiter.module").then((m) => m.WaiterModule),
+  },
+];
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
-  exports: [RouterModule]
+  exports: [RouterModule],
 })
-export class AppRoutingModule { }
+export class AppRoutingModule {}
