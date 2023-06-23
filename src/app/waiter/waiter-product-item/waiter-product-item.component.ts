5d4ffd5a97ea79e22db19e14c8abc2fc34961396
KhaledEllithy <khaledellessy310@gmail.com>
2023-06-23 21:40:55 +0300

    Create Component Product item

---
+++ src/app/waiter/waiter-product-item/waiter-product-item.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-waiter-product-item',
+  templateUrl: './waiter-product-item.component.html',
+  styleUrls: ['./waiter-product-item.component.css']
+})
+export class WaiterProductItemComponent {
+
+}
--- src/app/waiter/waiter.module.ts
+++ src/app/waiter/waiter.module.ts
@@ -1,12 +1,14 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { WaiterProductListComponent } from './waiter-product-list/waiter-product-list.component';
+import { WaiterProductItemComponent } from './waiter-product-item/waiter-product-item.component';
 
 
 
 @NgModule({
   declarations: [
-    WaiterProductListComponent
+    WaiterProductListComponent,
+    WaiterProductItemComponent
   ],
   imports: [
     CommonModule
