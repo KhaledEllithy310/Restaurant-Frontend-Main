968c07317cea5e6d85159f7816cc4282f6adbabb
KhaledEllithy <khaledellessy310@gmail.com>
2023-06-25 17:18:13 +0300

    Adding dashboard Component

--- src/app/admin/admin.module.ts
+++ src/app/admin/admin.module.ts
@@ -2,11 +2,15 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { AdminComponent } from './admin.component';
 import { RouterModule, Routes } from '@angular/router';
+import { DashboardComponent } from './dashboard/dashboard.component';
 
-const routes: Routes = [{}];
+const routes: Routes = [{
+  path: 'dashboard',
+  component: DashboardComponent
+}];
 
 @NgModule({
-  declarations: [AdminComponent],
+  declarations: [AdminComponent, DashboardComponent],
   imports: [CommonModule, RouterModule.forChild(routes)],
   exports: [AdminComponent],
 })
