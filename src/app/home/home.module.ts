605bad66139f89657901c401a6d4254badfe31b8
ayasamir <ayasamir689@gmail.com>
2023-06-28 23:56:02 +0300

    meet our team secation

--- src/app/home/home.module.ts
+++ src/app/home/home.module.ts
@@ -2,16 +2,17 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { HomeComponent } from '../home/home.component';
 import { HomeSection1Component } from './home-section1/home-section1.component';
+import { MeetOurTeamComponent } from './meet-our-team/meet-our-team.component';
 
 
 
 @NgModule({
   declarations: [
-    HomeComponent,HomeSection1Component
+    HomeComponent,HomeSection1Component,MeetOurTeamComponent
   ],
   imports: [
     CommonModule
   ],
-  exports:[HomeSection1Component]
+  exports:[HomeSection1Component,MeetOurTeamComponent]
 })
 export class HomeModule { }
---
+++ src/app/home/meet-our-team/meet-our-team.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-meet-our-team',
+  templateUrl: './meet-our-team.component.html',
+  styleUrls: ['./meet-our-team.component.css']
+})
+export class MeetOurTeamComponent {
+
+}
