bb088edef3e545f491dbd737f80885b459927c6d
ahmedmangood <ahmedmangood656@gmail.com>
2023-06-30 20:44:31 +0300

    get the users data from api and create the crud oprations

--- src/app/admin/users/add-users/add-users.component.ts
+++ src/app/admin/users/add-users/add-users.component.ts
@@ -1,4 +1,8 @@
+import { HttpHeaders } from '@angular/common/http';
 import { Component } from '@angular/core';
+import { FormBuilder, FormGroup, Validators } from '@angular/forms';
+import { User, UserRole } from 'src/app/interfaces/user';
+import { UsersService } from 'src/app/services/users.service';
 
 @Component({
   selector: 'app-add-users',
@@ -6,5 +10,66 @@ import { Component } from '@angular/core';
   styleUrls: ['./add-users.component.css']
 })
 export class AddUsersComponent {
+  user: User = {
+    name: '',
+    password: '',
+    email: '',
+    role: UserRole.Admin,
+    phone: ''
+  }
+  errors: any = [];
+  success!: string;
+  addUserForm!: FormGroup;
 
+  constructor(private addUserService: UsersService, private fb: FormBuilder){}
+
+  ngOnInit() {
+    this.addUserForm = this.fb.group({
+      username: ['', [Validators.required, Validators.minLength(3)]], 
+      password: ['', [Validators.required, Validators.minLength(8), Validators.pattern(/^(?=.*[A-Z])(?=.*[a-z])(?=.*\d).*$/)]],
+      role: ['', [Validators.required]],
+      email: [null, [Validators.required, Validators.email]], 
+      phoneNumber: [null, [Validators.required, Validators.pattern(/^(010|011|012)\d{8}$/)]] 
+    })
+  }
+
+  get registerForm() {
+    return this.addUserForm.controls;
+  }
+
+  onFileSelected(event: any){
+    const file = event.target.files[0];
+    if (file) {
+      this.user.image = file;
+    }
+  }
+
+  submitForm() {
+    const formData = new FormData();
+    formData.append('name', this.user.name);
+    formData.append('password', this.user.password);
+    formData.append('email', this.user.email);
+    formData.append('role', this.user.role);
+    if(this.user.image) {
+      formData.append('image', this.user.image, this.user.image?.name);
+    }
+    formData.append('phone', this.user.phone);
+    // console.log(formData);
+    const headers = new HttpHeaders({
+      'Accept': 'application/json',
+      'Content-Type': 'multipart/form-data'
+    });
+    
+    this.addUserService.addUser(formData, headers).subscribe({
+      next: (res: any) => {
+        this.success = res.message;
+        console.log(res, "response")
+      },
+      error: (err: any) => {
+        this.errors = err.error.errors;
+        console.log(err.error.errors, "errors");
+      }
+    })
+
+  }
 }
--- src/app/admin/users/edit-users/edit-users.component.ts
+++ src/app/admin/users/edit-users/edit-users.component.ts
@@ -1,5 +1,8 @@
+import { HttpHeaders } from '@angular/common/http';
 import { Component } from '@angular/core';
+import { FormBuilder, FormGroup, Validators } from '@angular/forms';
 import { ActivatedRoute, Route } from '@angular/router';
+import { User, UserRole } from 'src/app/interfaces/user';
 import { UsersService } from 'src/app/services/users.service';
 
 @Component({
@@ -10,13 +13,62 @@ import { UsersService } from 'src/app/services/users.service';
 
 export class EditUsersComponent {
 
-  user: any;
-  id: any;
-  constructor(private getUsers: UsersService, private route: ActivatedRoute) {}
 
-  ngOnInit() {
-    this.id = this.route.snapshot.paramMap.get('id');
-    this.getUsers.getUserByID(this.id).subscribe((res: any) => this.user = res.data)
+  errors: any = [];
+  success!: string;
+  editUserForm!: FormGroup;
+  olduser!: User;
+  image: any;
+  id: any;
+  
+  constructor(private userService: UsersService, private fb: FormBuilder, private route: ActivatedRoute){
+    this.editUserForm = this.fb.group({
+      name: ['', [Validators.required, Validators.minLength(3)]], 
+      password: ['', [Validators.required, Validators.minLength(8), Validators.pattern(/^(?=.*[A-Z])(?=.*[a-z])(?=.*\d).*$/)]],
+      role: ['', [Validators.required]],
+      email: [null, [Validators.required, Validators.email]], 
+      phone: [null, [Validators.required, Validators.pattern(/^(010|011|012)\d{8}$/)]] 
+    })
+  }
+  
+    ngOnInit() {
+      this.id = this.route.snapshot.paramMap.get('id');
+      this.userService.getUserByID(this.id).subscribe((res: any) => this.olduser = res.data)
+    }
+
+
+  get registerForm() {
+    return this.editUserForm.controls;
+  }
+
+  onFileSelected(event: any){
+    const file = event.target.files[0];
+    if (file) {
+      this.image = file;
+    }
+  }
+
+  submitForm() {
+    const formData = this.editUserForm.value
+    formData['image'] =  this.image;
+    formData['_method'] = 'put';
+    const headers = new HttpHeaders({
+      'Accept': 'application/json',
+      'Content-Type': 'multipart/form-data'
+    });
+    this.userService.editUserByID(this.id, formData, headers).subscribe(
+      {
+      next: (res: any) => {
+        this.success = res.message;
+        console.log(res, "response")
+      },
+      error: (err: any) => {
+        this.errors = err.error.errors;
+        console.log(err.error.errors, "errors");
+      }
+    }
+    )
+
   }
 
 }
