28207a82f2a32b1fa5f88deb1b98e4d1b10cf858
KhaledEllithy <khaledellessy310@gmail.com>
2023-07-03 15:58:26 +0300

    Adding Redirect To Product Details

--- src/app/admin/users/users.component.ts
+++ src/app/admin/users/users.component.ts
@@ -5,18 +5,18 @@ import { UsersService } from 'src/app/services/users.service';
 @Component({
   selector: 'app-users',
   templateUrl: './users.component.html',
-  styleUrls: ['./users.component.css']
+  styleUrls: ['./users.component.css'],
 })
 export class UsersComponent {
-
   users: any;
   success!: string;
   errors: any = [];
   constructor(private listusersService: UsersService, private router: Router) {}
 
-
   ngOnInit() {
-    this.listusersService.listUsers().subscribe((res: any) => this.users = res.data)
+    this.listusersService
+      .listUsers()
+      .subscribe((res: any) => (this.users = res.data));
   }
 
   reload() {
@@ -24,20 +24,19 @@ export class UsersComponent {
   }
 
   editUser(id: any) {
-    this.router.navigate([`admin/users/edit/${id}`])
+    this.router.navigate([`admin/dashboard/users/edit/${id}`]);
   }
   deleteUser(id: any) {
-      this.users = this.users?.filter((user:any) => user.id != id);
-      this.listusersService.deleteUserByID(id).subscribe({
-        next: (res: any) => {
-          this.success = res.message;
-          console.log(res, "response")
-        },
-        error: (err: any) => {
-          this.errors = err.error.errors;
-          console.log(err.error.errors, "errors");
-        }
-        
-      })
+    this.users = this.users?.filter((user: any) => user.id != id);
+    this.listusersService.deleteUserByID(id).subscribe({
+      next: (res: any) => {
+        this.success = res.message;
+        console.log(res, 'response');
+      },
+      error: (err: any) => {
+        this.errors = err.error.errors;
+        console.log(err.error.errors, 'errors');
+      },
+    });
   }
 }
