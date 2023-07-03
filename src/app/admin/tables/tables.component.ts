9712c588ee83a78cb233bc418820fb7b241afa34
ayasamir <ayasamir689@gmail.com>
2023-07-03 23:09:22 +0300

    create update2

--- src/app/admin/tables/tables.component.ts
+++ src/app/admin/tables/tables.component.ts
@@ -14,39 +14,35 @@ import { TablesService } from 'src/app/services/tables.service';
 })
 export class TablesComponent {
   guest_numbers: any;
-  Tables!:any;
+  Tables!: any;
   status: any;
   tableForm!: FormGroup;
   idtable!: number;
   selectedtable: any = {};
   updatetableForm!: FormGroup;
 
-
   // @ViewChild('addfileInput', { static: false }) addfileInput!: ElementRef;
   @ViewChild('addTables', { static: false }) addTables!: ElementRef;
 
   constructor(
-  private fb: FormBuilder,
-  private tableService: TablesService,
-    // private getCatService: GetDataService,
-    ){}
+    private fb: FormBuilder,
+    private tableService: TablesService // private getCatService: GetDataService,
+  ) {}
 
   ngOnInit() {
     //call the all table
     this.getAllTable();
     // to add  tableForm
     this.tableForm = this.fb.group({
-      TableNo:
-       ['', [Validators.required]],
+      TableNo: ['', [Validators.required]],
       guest_numbers: ['', [Validators.required]],
       status: [true],
     });
+
     this.updatetableForm = this.fb.group({
       TableNo_updated: ['', [Validators.required]],
       guest_numbers_updated: ['', [Validators.required]],
       status_updated: ['', [Validators.required]],
-
-
     });
   }
   get guest_numbers_Control() {
@@ -58,26 +54,24 @@ export class TablesComponent {
   get status_Control() {
     return this.tableForm.controls['status'];
   }
+
   get guest_numbers_updated_Control() {
-    return this.tableForm.controls['guest_numbers_updated'];
+    return this.updatetableForm.controls['guest_numbers_updated'];
   }
   get TableNo_updated_Control() {
-    return this.tableForm.controls['TableNo_updated'];
+    return this.updatetableForm.controls['TableNo_updated'];
   }
   get status_updated_Control() {
-    return this.tableForm.controls['status_updated'];
+    return this.updatetableForm.controls['status_updated'];
   }
 
   createTable() {
-
-
     const newTable = new FormData();
     newTable.append('number', this.tableForm.value.TableNo);
     newTable.append('guest_numbers', this.tableForm.value.guest_numbers);
     newTable.append('status', this.tableForm.value.status);
 
     console.log(newTable);
-
 
     this.tableService.addTables(newTable).subscribe(
       (response) => {
@@ -96,13 +90,12 @@ export class TablesComponent {
   //GET ALL TABLES
   getAllTable() {
     this.tableService.getTable().subscribe({
-    next: (res :any)=>{
-      this.Tables =res.data,
-      console.log(res)
-    },
-    error: (err:any)=>{
-      console.log(err);
-    }
+      next: (res: any) => {
+        (this.Tables = res.data), console.log(res);
+      },
+      error: (err: any) => {
+        console.log(err);
+      },
       // (response: any) => {
       //   this.TableNo = response.data;
       //   console.log(response);
@@ -114,124 +107,84 @@ export class TablesComponent {
     });
   }
 
-  // createTable(data: any) {
-  //   this.addService.createCategory(data).subscribe((res) => console.log(res));
-  //   this.getAllCategory();
-  //   window.location.reload();
-  // }
-
-  // deleteTable(index: any) {
-  //   let tables = document.getElementById(`cate${index}`);
-  //   tables?.remove();
-  //   let idtable = this.TableNo[index].id;
-  //   this.tableService.Deletetable(idtable).subscribe(
-  //     (response: any) => {
-  //       console.log(response);
-  //     },
-  //     (error) => {
-  //       console.log(error);
-  //     }
-  //   );
-  // }
-
   //GET THE ID OF THE OBJECT
   getIdtable(idtable: any) {
-       //Call The Old Category
-       console.log(idtable)
-       this.tableService
-       .getOldTable(idtable)
-       .subscribe((Response: any) => {
-         this.selectedtable = Response.data;
-         console.log(this.selectedtable.number);
-         //set the image of the old category to  {this.imageUrl} to display it in modal
-         //set the name of the old category to the input field value {name_updated}
-         TableNo_updated.value = this.selectedtable.number;
-       });
+    //Call The Old Category
+    console.log(idtable);
+    this.tableService.getOldTable(idtable).subscribe((Response: any) => {
+      this.selectedtable = Response.data;
+      console.log(this.selectedtable.number);
+      //set the image of the old category to  {this.imageUrl} to display it in modal
+      //set the name of the old category to the input field value {name_updated}
+      TableNo_updated.value = this.selectedtable.number;
+      guest_numbers_updated.value = this.selectedtable.guest_numbers;
+    });
 
-     //store the idCategory in {this.idCategory} to pass it to the modal
-     this.idtable = idtable;
+    //store the idCategory in {this.idCategory} to pass it to the modal
+    this.idtable = idtable;
 
-     //Select the input field name_updated
-     let TableNo_updated = document.getElementById(
-       'TableNo_updated'
-     ) as HTMLInputElement;
+    //Select the input field name_updated
+    let TableNo_updated = document.getElementById(
+      'TableNo_updated'
+    ) as HTMLInputElement;
+    let guest_numbers_updated = document.getElementById(
+      'guest_numbers_updated'
+    ) as HTMLInputElement;
 
-     console.log(TableNo_updated.value);
-
+    console.log(TableNo_updated.value);
   }
 
-  updatetable( idtable: any) {
-    this.tableService
-    .getOldTable(idtable)
-    .subscribe((Response: any) =>{
+  updatetable(idtable: any) {
+    this.tableService.getOldTable(idtable).subscribe((Response: any) => {
       this.selectedtable = Response.data;
       console.log(this.selectedtable.TableNo);
-
-
     });
-     const updatetable = new FormData();
-     updatetable.set(
-      'TableNo_updated',
-      this.updatetableForm.value.TableNo_updated || this.selectedtable.TableNo
-    );
-    console.log(updatetable.get('TableNo_updated'));
-    this.tableService.UpdateTable(updatetable, idtable).subscribe(
+    // const updatetable = new FormData();
+    // updatetable.append(
+    //   'number',
+    //   this.updatetableForm.value.TableNo_updated || this.selectedtable.number
+    // );
+    // updatetable.append(
+    //   'guest_numbers',
+    //   this.updatetableForm.value.guest_numbers_updated ||
+    //     this.selectedtable.guest_numbers
+    // );
+    // updatetable.append(
+    //   'status',
+    //   this.updatetableForm.value.status_updated || this.selectedtable.status
+    // );
+    ////console.log(updatetable.get('TableNo_updated'));
+
+    const updatetable2 = {
+      number:
+        this.updatetableForm.value.TableNo_updated || this.selectedtable.number,
+      guest_numbers:
+        this.updatetableForm.value.guest_numbers_updated ||
+        this.selectedtable.guest_numbers,
+      status:
+        this.updatetableForm.value.status_updated || this.selectedtable.status,
+    };
+
+    // console.log(updatetable.get('number'));
+    // console.log(updatetable.get('guest_numbers'));
+    // console.log(updatetable.get('status'));
+
+    this.tableService.UpdateTable(updatetable2, idtable).subscribe(
       (response) => {
         console.log(response);
       },
       (error) => {
         console.log(error);
       }
-      );
-      console.log(updatetable.get('TableNo'));
-      this.tableService.UpdateTable(updatetable,idtable).subscribe(
-        (response) =>{
-          console.log(response);
-        },
-        (error =>{
-          console.log(error);
-        })
-      );
-      window.location.reload();
-    //   .updateCategory(idCategory, data)
-    //   .subscribe((res) => console.log(res));
+    );
     // window.location.reload();
   }
 
-  //CREATE Table
+  change_status(id_Product: any) {
+    console.log(id_Product);
 
-
-  createTable1() {
-    // const formData = new FormData();
-    // formData.set('TableNo', this.TableNo);
-    // formData.set('guest_numbers', this.guest_numbers);
-    // formData.set('status', this.status);
-    // console.log(formData.get('TableNo'));
-    // console.log(formData.get('guest_numbers'));
-    // console.log(formData.get('status'));
-
-    // this.addService.createBook(formData).subscribe((res) => console.log(res));
-    // this.getAllBook();
-    // console.log(data);
-    // window.location.reload();
-  }
-
-  //Update Category
-  // name_updated: any;
-  // image_updated: any;
-
-  updateTable() {
-    // const image_updated = this.updatefileInput.nativeElement.files[0];
-    // console.log('image_updated', image_updated);
-    // const formData = new FormData();
-    // formData.set('name_updated', this.name_updated);
-    // formData.append('image_updated', image_updated);
-    // console.log(formData.get('name_updated'));
-    // console.log(formData.get('image_updated'));
-    // this.addService.createBook(formData).subscribe((res) => console.log(res));
-    // this.getAllBook();
-    // console.log(data);
-    // window.location.reload();
-
+    this.tableService
+      .change_status(id_Product)
+      .subscribe((Response) => console.log(Response));
   }
 }
--- src/app/services/tables.service.ts
+++ src/app/services/tables.service.ts
@@ -2,32 +2,31 @@ import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
 
 @Injectable({
-  providedIn: 'root'
+  providedIn: 'root',
 })
 export class TablesService {
+  constructor(private http: HttpClient) {}
 
-  constructor( private http: HttpClient) { }
-  
-  addTables(data:any) {
-    return this.http.post('http://127.0.0.1:8000/api/tables/' , data);
-    }
-    getTable() {
-      return this.http.get('http://127.0.0.1:8000/api/tables/');
-    }
-  
-    
+  addTables(data: any) {
+    return this.http.post('http://127.0.0.1:8000/api/tables/', data);
+  }
+  getTable() {
+    return this.http.get('http://127.0.0.1:8000/api/tables/');
+  }
+
   // AddCategory(data: any) {
   //   return this.http.post('http://127.0.0.1:8000/api/category', data);
   // }
 
- 
-
   UpdateTable(data: any, id: any) {
-    console.log(data.get('TableNo'));
     return this.http.put('http://127.0.0.1:8000/api/tables/' + id, data);
   }
 
   getOldTable(id: any) {
-    return this.http.get('http://127.0.0.1:8000/api/tables/' + id );
+    return this.http.get('http://127.0.0.1:8000/api/tables/' + id);
+  }
+
+  change_status(id: any) {
+    return this.http.get('http://127.0.0.1:8000/api/tables/status/' + id);
   }
 }
