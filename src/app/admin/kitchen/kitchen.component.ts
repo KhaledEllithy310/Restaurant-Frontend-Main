8b7261dfe628149b1f42b4a57204c928871fab0b
ahmedmangood <ahmedmangood656@gmail.com>
2023-07-04 19:26:51 +0300

    connect kitchen with api

--- src/app/admin/kitchen/kitchen.component.ts
+++ src/app/admin/kitchen/kitchen.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { KitchenService } from 'src/app/services/kitchen.service';
 
 @Component({
   selector: 'app-kitchen',
@@ -7,4 +8,68 @@ import { Component } from '@angular/core';
 })
 export class KitchenComponent {
 
+  orders: any;
+  count: any;
+  completedSuccess: any;
+  errorComplete: any;
+  canceledSuccess: any;
+  errorCanceled: any;
+
+  constructor(private kitchen: KitchenService) {}
+
+  ngOnInit() {
+    this.kitchen.getOrders().subscribe((res: any) => this.orders = res.data);
+  }
+
+  getQuantity(productId: number, orderProducts: any[]) {
+    const product = orderProducts.find(p => p.product_id === productId);
+    return product ? product.quantity : 0;
+  }
+  
+  getOrderProductId(productId: number, orderProducts: any[]) {
+    const orderProduct = orderProducts.find(op => op.product_id === productId);
+    return orderProduct ? orderProduct.id : null;
+  }
+
+  getOrderStatus(orderProducts: any[]) {
+    const completedProducts = orderProducts.filter((op: { status: string; }) => op.status === "Complete");
+    return completedProducts.length;
+  }
+
+  complete(orderID: any, orderProductID: any) {
+    this.kitchen.completed(orderID, orderProductID).subscribe({
+      next: (res: any) => {
+        this.completedSuccess = res;
+        console.log(res);
+        const order = this.orders.find((o: any) => o.id === orderID);
+        const orderProduct = order.order_products.find((op: any) => op.id === orderProductID);
+        if (orderProduct) {
+          orderProduct.status = 'Complete'; // update the status in the local data
+          this.getOrderStatus(order.order_products); // update the completed products count
+        }
+      },
+      error: (err: any) => {
+        this.errorComplete = err.errors.error;
+        console.log(err);
+      }
+    })
+  }
+  canceledOrder(orderID: any, orderProductID: any) {
+    this.kitchen.canceled(orderID, orderProductID).subscribe({
+      next: (res: any) => {
+        this.canceledSuccess = res;
+        console.log(res);
+        const order = this.orders.find((o: any) => o.id === orderID);
+        const orderProduct = order.order_products.find((op: any) => op.id === orderProductID);
+        if (orderProduct) {
+          orderProduct.status = 'Canceled'; // update the status in the local data
+          this.getOrderStatus(order.order_products); // update the completed products count
+        }
+      },
+      error: (err: any) => {
+        this.errorCanceled = err.errors.error;
+        console.log(err);
+      }
+    })
+  }
 }
