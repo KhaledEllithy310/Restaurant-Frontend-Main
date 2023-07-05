8e4e17df3b67d0f514481f063995a979a8aee71e
KhaledEllithy <khaledellessy310@gmail.com>
2023-07-05 03:30:53 +0300

    Delete Ingredients

---
+++ src/app/admin/products/add-sub-ingredient/add-sub-ingredient.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-add-sub-ingredient',
+  templateUrl: './add-sub-ingredient.component.html',
+  styleUrls: ['./add-sub-ingredient.component.css']
+})
+export class AddSubIngredientComponent {
+
+}
--- src/app/admin/products/product-details/product-details.component.ts
+++ src/app/admin/products/product-details/product-details.component.ts
@@ -2,6 +2,8 @@ import { Component } from '@angular/core';
 import { ActivatedRoute, Router } from '@angular/router';
 import { IngridentsService } from 'src/app/services/ingridents.service';
 import { ProductsService } from 'src/app/services/products.service';
+import { Ingrident } from './../../../interfaces/ingrident';
+import { FormBuilder, FormGroup, Validators } from '@angular/forms';
 
 @Component({
   selector: 'app-product-details',
@@ -11,18 +13,25 @@ import { ProductsService } from 'src/app/services/products.service';
 export class ProductDetailsComponent {
   targetProduct: any;
   targetExtraList: any[] = [];
+  targetIngredientList: any[] = [];
+  ingredientsForm!: FormGroup;
+
   constructor(
     private activatedRoute: ActivatedRoute,
     private router: Router,
     private productsService: ProductsService,
-    private IngridentsService: IngridentsService
+    private IngridentsService: IngridentsService,
+    private fb: FormBuilder
   ) {}
 
   ngOnInit() {
     //Call All Product And Display The Target Product
     this.getTargetProduct();
-    //call all extra for target product
-    // this.getTargetExtra();
+
+    this.ingredientsForm = this.fb.group({
+      Ingredient_id: ['', [Validators.required]],
+      Ingredient_Quantity: ['', [Validators.required]],
+    });
   }
 
   //Call All Product And Display The Target Product
@@ -30,19 +39,12 @@ export class ProductDetailsComponent {
     let targetId = this.activatedRoute.snapshot.params['id'];
     this.productsService.getProductById(targetId).subscribe((Response: any) => {
       (this.targetProduct = Response.data), console.log(this.targetProduct);
-      console.log(this.targetProduct.extra);
+      console.log(this.targetProduct);
       this.getTargetExtra();
+      this.getTargetIngredient();
     });
   }
 
-  // //Call All Product And Display The Target Product
-  // getTargetProduct1() {
-  //   this.productsService.getAllProduct().subscribe((Response: any) => {
-  //     this.targetProduct = Response.data.find(
-  //       (elem: any) => elem.id == this.activatedRoute.snapshot.params['id']
-  //     );
-  //   });
-  // }
   getTargetExtra() {
     for (const key in this.targetProduct.extra) {
       if (this.targetProduct.extra.hasOwnProperty(key)) {
@@ -52,5 +54,40 @@ export class ProductDetailsComponent {
         console.log(this.targetExtraList);
       }
     }
+  }
+
+  deleteIngredient(idIngredient: any) {
+    this.productsService.deleteIngredient(idIngredient);
+    this.productsService.getIngredientsList().subscribe((response: any) => {
+      this.targetIngredientList = response;
+      console.log(this.targetIngredientList);
+    });
+  }
+
+  getTargetIngredient() {
+    let TargetIngredient = [];
+    for (let i = 0; i < this.targetProduct.ingredients.length; i++) {
+      const Ingrident = this.targetProduct.ingredients[i];
+
+      const newIngredient = {
+        id: Ingrident.pivot.ingredient_id,
+        name: Ingrident.name,
+        quantity: Ingrident.pivot.quantity,
+        total: Ingrident.pivot.total,
+      };
+
+      TargetIngredient.push(newIngredient);
+    }
+    this.productsService.initializeIngredientList(TargetIngredient);
+
+    this.productsService.getIngredientsList().subscribe(
+      (response: any) => {
+        this.targetIngredientList = response;
+        console.log(this.targetIngredientList);
+      },
+      (error) => {
+        console.log(error);
+      }
+    );
   }
 }
