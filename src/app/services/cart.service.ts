76154fcd344b659a0960535e09bc6db1fb907fa4
KhaledEllithy <khaledellessy310@gmail.com>
2023-07-06 09:54:07 +0300

    Adding Style For Cart

--- src/app/services/cart.service.ts
+++ src/app/services/cart.service.ts
@@ -6,6 +6,7 @@ import { BehaviorSubject } from 'rxjs';
 })
 export class CartService {
   private cartProducts = new BehaviorSubject<any[]>([]);
+  private numberProductInCart = new BehaviorSubject(0);
   constructor() {}
 
   //STORE ITEM IN ARRAY OF CART {cartProducts}
@@ -18,5 +19,20 @@ export class CartService {
   // Display Cart Product
   getCartProducts() {
     return this.cartProducts.asObservable();
+  }
+
+  //Get NUmber Product from Cart
+  getNumProductInCart() {
+    return this.numberProductInCart.asObservable();
+  }
+
+  //Set NUmber Product in Cart
+  setNumProductInCart(newVal: any) {
+    this.numberProductInCart.next(newVal);
+  }
+
+  plusProduct(){
+
+
   }
 }
